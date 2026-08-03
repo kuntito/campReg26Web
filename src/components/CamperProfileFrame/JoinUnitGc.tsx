c61a52a54b084a9d07c63c597bbdb6327e94c4af
kuntito <owolabitito@gmail.com>
2026-08-03 14:24:06 +0100

    add comp, join unit group chat

---
+++ src/components/CamperProfileFrame/JoinUnitGc.tsx
@@ -0,0 +1,57 @@
+import { HStack, Text, VStack } from "@chakra-ui/react"
+import AppIconButton from "../util/AppIconButton"
+import { WhatsappIcon } from "../icons/WhatsappIcon"
+
+interface Props {
+    link: string;
+}
+
+const JoinUnitGc = ({
+    link
+}: Props) => {
+    return (
+        <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <VStack
+                gap={"2px"}
+                w={"100%"}
+            >
+                <HStack
+                    bg={"palette.raze"}
+                    color={"palette.life"}
+                    gap={"10px"}
+                    paddingX={"16px"}
+                    paddingY={"6px"}
+                    borderRadius={50}
+                    cursor={"pointer"}
+                    _hover={{
+                        opacity: 0.7
+
+                    }}
+                    _active={{
+                        opacity: 0.5
+                    }}
+                    transition="opacity 0.2s ease"
+                >
+                    <WhatsappIcon />
+                    <Text
+                        textStyle={"orion"}
+                    >
+                        join the group
+                    </Text>
+                </HStack>
+                <Text
+                    color={"palette.skylar"}
+                    textStyle={"hush"}
+                >
+                    stay updated
+                </Text>
+            </VStack>
+        </a>
+    )
+}
+
+export default JoinUnitGc
